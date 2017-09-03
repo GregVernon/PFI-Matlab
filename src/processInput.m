07ebaaa1208ddf4686bc39c72fb37f96cc55388b
GregVernon <greg.j.vernon@gmail.com>
2017-09-03 18:26:31 -0500

    Input-deck to graph
    Input processor converts input-deck into a graph structure.

--- src/processInput.m
+++ src/processInput.m
@@ -5,33 +5,110 @@ PARAM = generateLegalParameters;
 fLines = fileread(inFile);
 
 % Separate the file by individual lines
-fLines = deblank(strsplit(fLines,'\n')');
+fLines = strip(strsplit(fLines,'\n')');
 
 %% Convert each line to UPPERCASE
 % Compatibility between 2016a and 2016b
 if verLessThan('matlab','9.2')
-  for ii = 1:length(fLines)
-    fLines{ii} = upper(fLines{ii});
-  end
+    for ii = 1:length(fLines)
+        fLines{ii} = upper(fLines{ii});
+    end
 else
-  fLine = upper(fLines);
+    fLine = upper(fLines);
 end
 
-%% Find "BEGIN" and "END" sections
-n1 = 0;
-n2 = 0;
-for ii = 1:length(fLines)
-  if regexp(fLines{ii},'BEGIN')
-    n1 = n1+1;
-    parseLine = strsplit(fLines{ii});
-    sectionType{1,n1,1} = parseLine{end};
-    sectionType{1,n1,2} = ii;
-  elseif regexp(fLines{ii},'END')
-    n2 = n2+1;
-    parseLine = strsplit(fLines{ii});
-    sectionType{2,n2,1} = parseLine{end};
-    sectionType{2,n2,2} = ii;
-  end
+%% Create Structure
+depth = 0;
+sCount = 0;
+for f = 1:length(fLines)
+    if regexp(fLines{f},'BEGIN')
+        sCount = sCount + 1;
+        depth = depth + 1;
+        sParts = string(strsplit(fLines{f},' '));
+        sType(sCount,1).Name = sParts(end);
+        sType(sCount,1).Depth = depth;
+        sType(sCount,1).startLine = f;
+    elseif regexp(fLines{f},'END')
+        depth = depth - 1;
+    end
 end
 
-input = [];
+%% Find each section's end lines
+for ii = 1:length(sType)
+    endString = ["END" + " " + sType(ii).Name];
+    searchLines = fLines(sType(ii).startLine:end);
+    endLines = strcmpi(endString,searchLines);
+    endLine = find(endLines,1,'first');
+    sType(ii).endLine = (sType(ii).startLine -1) + endLine;
+end
+
+%% Make unique node names
+[uNames, uIdx] = unique([sType.Name]);
+isDuplicate = ~ismember([1:length(sType)],uIdx);
+dNames = unique([sType(isDuplicate).Name]);
+for ii = 1:length(dNames)
+    fDuplicates = find(strcmpi([sType.Name],dNames(ii)));
+    for jj = 1:length(fDuplicates)
+        sType(fDuplicates(jj)).Name = dNames(ii) + "_" + num2str(jj);
+    end
+end
+
+%% Extract section field-value pairs
+for ii = 1:length(sType)
+    startLine = sType(ii).startLine;
+    endLine = sType(ii).endLine;
+    
+    % Remove lines above section
+    notSectionLines = [1:startLine];
+    % Remove lines below section
+    notSectionLines = [notSectionLines [endLine:length(fLines)]];
+    % Remove empty lines
+    notSectionLines = [notSectionLines find(fLines == "")'];
+    for jj = 1:length(sType)
+        if ii == jj
+            continue
+        else
+            % Remove lines in enclosed subsections
+            if sType(jj).startLine > startLine && sType(jj).endLine < endLine
+                notSectionLines = [notSectionLines [sType(jj).startLine:sType(jj).endLine]];
+            end
+        end
+    end
+    SectionLines = fLines;
+    SectionLines(notSectionLines) = [];
+    
+    if ~isempty(SectionLines)
+        for p = 1:length(SectionLines)
+            fvp = strsplit(SectionLines{p},"=");
+            sType(ii).field(p) = fvp(1);
+            sType(ii).value(p) = fvp(2);
+        end
+    end
+end
+
+%% Create Graph of Simulation Input Structure
+% Define NodeTable
+for ii = 1:length(sType)
+    nodeName{ii,1} = char(sType(ii).Name);
+    nodeFields{ii,1} = sType(ii).field';
+    nodeValues{ii,1} = sType(ii).value';
+end
+% Define Edges
+eCount = 0;
+for ii = 2:length(sType)
+    foundParent = false;
+    jj = 0;
+    while foundParent == false
+        jj = jj+1;
+        if sType(ii-jj).Depth < sType(ii).Depth
+            foundParent = true;
+            eCount = eCount+1;
+            edgeNodes(eCount,:) = [ii ii-jj];
+        end
+    end
+end
+EdgeTable = table(edgeNodes,'VariableNames',{'EndNodes'});
+NodeTable = table(nodeName,nodeFields,nodeValues,'VariableNames',{'Name','Field','Value'});
+inGraph = graph(EdgeTable,NodeTable);
+plot(inGraph)
+end
